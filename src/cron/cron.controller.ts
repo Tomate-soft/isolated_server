c5d98a82b7efa551e70adffff8cffde95b7574f8
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-07-14 11:56:30 -0600

    building fix

--- src/cron/cron.controller.ts
+++ src/cron/cron.controller.ts
@@ -1,39 +1,39 @@
-import { Body, Controller, Get, NotFoundException, Post } from '@nestjs/common';
+import { Controller} from '@nestjs/common';
 import { CronService } from './cron.service';
 
 @Controller('cron')
 export class CronController {
   constructor(private readonlycronService: CronService) {}
 
-  @Get('test')
-  async test() {
-    try {
-      const res = await this.readonlycronService.initializeCronJobs();
-      return res;
-    } catch (error) {
-      throw new NotFoundException(`Ha ocurrido algo inesperado. Mas informacion: ${error}`);
-    }
-  }
+  // @Get('test')
+  // async test() {
+  //   try {
+  //     const res = await this.readonlycronService.initializeCronJobs();
+  //     return res;
+  //   } catch (error) {
+  //     throw new NotFoundException(`Ha ocurrido algo inesperado. Mas informacion: ${error}`);
+  //   }
+  // }
 
-  @Post('close-manual-period')
-  async closeManualPeriod(@Body() body: any) {
-    try {
-      const res = await this.readonlycronService.closeManualPeriod(body);
-      return res;
-    } catch (error) {
-      throw new NotFoundException(`Ha ocurrido algo inesperado. Mas informacion: ${error}`);
-    }
-  }
+  // @Post('close-manual-period')
+  // async closeManualPeriod(@Body() body: any) {
+  //   try {
+  //     const res = await this.readonlycronService.closeManualPeriod(body);
+  //     return res;
+  //   } catch (error) {
+  //     throw new NotFoundException(`Ha ocurrido algo inesperado. Mas informacion: ${error}`);
+  //   }
+  // }
 
-  @Get('create-manual-period')
-  async createManualPeriod() {
-    try {
-      const res = await this.readonlycronService.createPeriodForBranch();
-      return res;
-    } catch (error) {
-      throw new NotFoundException(`Ha ocurrido algo inesperado. Mas informacion: ${error}`);
-    }
-  }
+  // @Get('create-manual-period')
+  // async createManualPeriod() {
+  //   try {
+  //     const res = await this.readonlycronService.createPeriodForBranch();
+  //     return res;
+  //   } catch (error) {
+  //     throw new NotFoundException(`Ha ocurrido algo inesperado. Mas informacion: ${error}`);
+  //   }
+  // }
 
   // @Post('create-source-period')
   // async createSourcePeriod() {
