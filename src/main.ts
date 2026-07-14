656d807e7ce211c59d1b3b9b6ff7f2c1eb94b5ba
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2026-07-14 12:23:43 -0600

    add app domain

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5174',
+      'https://app.tomatesoft.com',
       'http://localhost:5173',
       'http://localhost:3000',
       'https://tomatesoft.com',
